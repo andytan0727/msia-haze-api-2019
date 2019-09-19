ca9839deec60764a028d8a70c1f67b4b8287c256
Andy Tan <andytan0727@gmail.com>
2019-09-19 12:27:35 +0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { onMount } from "svelte";
+  export let name;
+  export let date;
+
+  onMount(async () => {
+    const res = await fetch("/api/getApi");
+    const newDate = await res.text();
+    date = newDate;
+  });
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <h2>The date according to Node.js is:</h2>
+  <p>{date ? date : 'Loading date...'}</p>
+</main>
