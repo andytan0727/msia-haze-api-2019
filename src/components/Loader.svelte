563fa643defa095082dc198ae5f0a12e9e3a08a6
Andy Tan <andytan0727@gmail.com>
2019-09-20 15:38:39 +0800

    feat: create loader and navbar

---
+++ src/components/Loader.svelte
@@ -0,0 +1,27 @@
+<style>
+  .loader {
+    width: 120px;
+    height: 120px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    z-index: 1;
+    margin: -75px 0 0 -75px;
+    border: 11px solid #f3f3f3;
+    border-radius: 50%;
+    border-top: 11px solid #77dd62;
+
+    animation: spin 2s linear infinite;
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
+
+<div class="loader" />
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,10 @@
+<style>
+  .navbar-brand {
+    font-family: "Lora", serif;
+    font-weight: bold;
+  }
+</style>
+
+<nav class="navbar navbar-light p-4">
+  <a class="navbar-brand" href="/">M'sia Haze API</a>
+</nav>
