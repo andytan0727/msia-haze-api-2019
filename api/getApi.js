3071c9e9d96ab0b55823a2047626660297f22f7e
Andy Tan <andytan0727@gmail.com>
2019-09-20 15:37:10 +0800

    chore: add backend api

--- api/getApi.js
+++ api/getApi.js
@@ -1,4 +1,13 @@
-module.exports = (req, res) => {
-  const date = new Date().toString();
-  res.status(200).send(date);
+import axios from "axios";
+
+module.exports = async (req, res) => {
+  try {
+    const result = await axios.get(
+      "http://apims.doe.gov.my/data/public/CAQM/last24hours.json"
+    );
+    res.status(200).json(result.data);
+  } catch (error) {
+    console.log("error fetching api: ", error);
+    return res.status(404).json(error);
+  }
 };
